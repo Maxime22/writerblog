29398fa494b6097d6961913b39c60e06005c64e8
unknown <sabouretmaxime@gmail.com>
2019-02-18 12:04:42 +0100

    we add the function getController who check the XML routes, add them to the router then create a instance of the controller corresponding to the routefrom the URL

--- lib/MiniFram/Application.php
+++ lib/MiniFram/Application.php
@@ -30,4 +30,45 @@ abstract class Application
     {
         return $this->name;
     }
+
+    public function getController()
+    {
+        $router = new Router;
+
+        $xml = new \DOMDocument;
+        $xml->load(__DIR__ . '/../../App/' . $this->name . '/Config/routes.xml');
+
+        $routes = $xml->getElementsByTagName('route');
+
+        // We run the file xml associated to the name of the module
+        foreach ($routes as $route) {
+            $vars = [];
+
+            // We check if there are some variables in the module
+            if ($route->hasAttribute('vars')) {
+                $vars = explode(',', $route->getAttribute('vars'));
+            }
+
+            // We add all the routes of the XML to the router
+            $router->addRoute(new Route($route->getAttribute('url'), $route->getAttribute('module'), $route->getAttribute('action'), $vars));
+        }
+
+        try
+        {
+            // We get the route corresponding to the URL (now the router has all the routes)
+            $matchedRoute = $router->getRoute($this->httpRequest->requestURI());
+        } catch (\RuntimeException $e) {
+            if ($e->getCode() == Router::NO_ROUTE) {
+                // If there are no routes => Error 404
+                $this->httpResponse->redirect404();
+            }
+        }
+
+        // We add the variables of the URL the the $_GET table
+        $_GET = array_merge($_GET, $matchedRoute->vars());
+
+        // We create the controller due to our structure
+        $controllerClass = 'App\\' . $this->name . '\\Modules\\' . $matchedRoute->module() . '\\' . $matchedRoute->module() . 'Controller';
+        return new $controllerClass($this, $matchedRoute->module(), $matchedRoute->action());
+    }
 }
