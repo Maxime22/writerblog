ea197c5c1baf006920ae728f03d5987f7fa0a81c
unknown <sabouretmaxime@gmail.com>
2019-02-23 14:36:55 +0100

    some style changes

--- App/Frontend/Modules/Chapter/Views/index.php
+++ App/Frontend/Modules/Chapter/Views/index.php
@@ -9,12 +9,21 @@
 </figure>
 <div class="container">
 <h3>Bienvenue à toi cher lecteur...</h3>
+<p>
+Hoc inmaturo interitu ipse quoque sui pertaesus excessit e vita aetatis nono anno atque vicensimo cum quadriennio imperasset. natus apud Tuscos in Massa Veternensi, patre Constantio Constantini fratre imperatoris, matreque Galla sorore Rufini et Cerealis, quos trabeae consulares nobilitarunt et praefecturae.
+</p>
+<p>
+Denique Antiochensis ordinis vertices sub uno elogio iussit occidi ideo efferatus, quod ei celebrari vilitatem intempestivam urgenti, cum inpenderet inopia, gravius rationabili responderunt; et perissent ad unum ni comes orientis tunc Honoratus fixa constantia restitisset.
+</p>
+<p>
+Cum saepe multa, tum memini domi in hemicyclio sedentem, ut solebat, cum et ego essem una et pauci admodum familiares, in eum sermonem illum incidere qui tum forte multis erat in ore. Meministi enim profecto, Attice, et eo magis, quod P. Sulpicio utebare multum, cum is tribunus plebis capitali odio a Q. Pompeio, qui tum erat consul, dissideret, quocum coniunctissime et amantissime vixerat, quanta esset hominum vel admiratio vel querella.
+</p>
 <?php
-foreach ($listChapters as $chapter) {
+/* foreach ($listChapters as $chapter) {
     ?>
   <h2><a href="chapter-<?=$chapter['id']?>.html"><?=$chapter['title']?></a></h2>
   <p><?=nl2br($chapter['content'])?></p>
   <?php
-}?>
+} */?>
 </div>
 </div>
