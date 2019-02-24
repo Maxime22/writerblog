f0e7fda7e61af1f433e0eef787e03ef3decc00b5
unknown <sabouretmaxime@gmail.com>
2019-02-24 19:28:17 +0100

    table of views added

---
+++ App/Backend/Modules/Chapter/Views/index.php
@@ -0,0 +1,14 @@
+<p style="text-align: center">Il y a actuellement <?=$numberOfChapters?> chapitres. En voici la liste :</p>
+
+<table>
+  <tr><th>Auteur</th><th>Titre</th><th>Date d'ajout</th><th>Dernière modification</th><th>Action</th></tr>
+<?php
+foreach ($listChapters as $chapter) {
+    echo '<tr><td>', $chapter['author'], '</td><td>', $chapter['title'],
+    '</td><td>le ', $chapter['addDate']->format('d/m/Y à H\hi'), '</td><td>',
+    ($chapter['addDate'] == $chapter['modifDate'] ? '-' : 'le ' . $chapter['modifDate']->format('d/m/Y à H\hi')),
+    '</td><td><a href="chapter-update-', $chapter['id'], '><img src="/images/update.png" alt="Modifier" /></a> <a href="chapter-delete-',
+    $chapter['id'], '.html"><img src="/images/delete.png" alt="Supprimer" /></a></td></tr>', "\n";
+}
+?>
+</table>
