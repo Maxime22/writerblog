6603dd7d9d67fe7a989fda0a3c71ae0c35cc8e12
unknown <sabouretmaxime@gmail.com>
2019-02-24 19:46:04 +0100

    function processForm added in the index of the ChapterController (backend), this function write the chapter in the DB

--- App/Backend/Modules/Chapter/ChapterController.php
+++ App/Backend/Modules/Chapter/ChapterController.php
@@ -15,4 +15,28 @@ class ChapterController extends BackController
         $this->page->addVar('listChapters', $manager->getList());
         $this->page->addVar('numberOfChapters', $manager->count());
     }
+
+    public function processForm(HTTPRequest $request)
+    {
+        $chapter = new Chapter([
+            'author' => $request->postData('author'),
+            'title' => $request->postData('title'),
+            'content' => $request->postData('content'),
+        ]);
+
+        // The id of the chapter is sent if we want to modify it
+        if ($request->postExists('id')) {
+            $chapter->setId($request->postData('id'));
+        }
+
+        if ($chapter->isValid()) {
+            $this->managers->getManagerOf('Chapter')->save($chapter);
+
+            $this->app->user()->setFlash($chapter->isNew() ? 'Le chapitre a bien été ajouté!' : 'Le chapitre a bien été modifié !');
+        } else {
+            $this->page->addVar('erreurs', $chapter->errors());
+        }
+
+        $this->page->addVar('news', $chapter);
+    }
 }
