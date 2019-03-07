d23f292281c609cc2450bd1159e8bbdfb5c0f0da
unknown <sabouretmaxime@gmail.com>
2019-03-07 20:40:18 +0100

    changed balise by tag (names)

--- Web/js/book.js
+++ Web/js/book.js
@@ -4,45 +4,45 @@ let firstPage = document.getElementById('firstPage');
 let insideFirstPage = firstPage.firstChild
 
 let textToAddToSvg = ''
-let svgTextAndBalises = [] // should also register the spaces
-let svgBalise = []
-let singleBalise = ""
-let baliseNotToRegister = false;
+let svgTextAndTags = [] // should also register the spaces
+let svgTag = []
+let singleTag = ""
+let tagNotToRegister = false;
 let cmptString = 0;
-let weEnteredInABalise = false
+let weEnteredInATag = false
 let pageWhereToAddContent = insideFirstPage
 let weHadABR = false;
 weAreOnTheFirstPage = true;
 let contentDivHtml = contentDiv.innerHTML
 let lengthContent = contentDiv.innerHTML.length
 let regexSpace = new RegExp(/^\s+$/)
-let needToAddSvgBaliseInNewSvgTextAndBalise = false
+let needToAddSvgTagInNewSvgTextAndTag = false
 
 for (let index = 0; index < lengthContent; index++) { // on parcourt chaque caractère de ce qui est écrit dans content
-    singleBalise.includes('script') ? singleBalise = "" : ""; // being careful with the JS
+    singleTag.includes('script') ? singleTag = "" : ""; // being careful with the JS
 
-    if (needToAddSvgBaliseInNewSvgTextAndBalise) { // for the new pages (starting from the 2nd), we put all the balise who were still opened in the start of our new table svgTextAndBalises
-        needToAddSvgBaliseInNewSvgTextAndBalise = false
-        for (let index = 0; index < svgBalise.length; index++) {
-            svgTextAndBalises[index] = svgBalise[index];
+    if (needToAddSvgTagInNewSvgTextAndTag) { // for the new pages (starting from the 2nd), we put all the Tag who were still opened in the start of our new table svgTextAndTags
+        needToAddSvgTagInNewSvgTextAndTag = false
+        for (let index = 0; index < svgTag.length; index++) {
+            svgTextAndTags[index] = svgTag[index];
 
         }
-        svgBalise.length = 0
+        svgTag.length = 0
     }
 
-    if (weEnteredInABalise) {
-        singleBalise = singleBalise + contentDivHtml[index]
+    if (weEnteredInATag) {
+        singleTag = singleTag + contentDivHtml[index]
     }
 
-    if (singleBalise === '</') {
-        baliseNotToRegister = true;
-        svgBalise.pop() // delete the last element of the table
+    if (singleTag === '</') {
+        tagNotToRegister = true;
+        svgTag.pop() // delete the last element of the table
     }
 
-    if (singleBalise === '<br>') {
+    if (singleTag === '<br>') {
         weHadABR = true;
-        weEnteredInABalise = false;
-        singleBalise = ""
+        weEnteredInATag = false;
+        singleTag = ""
 
         for (let index = 0; index < 82; index++) {
             cmptString = cmptString + 1 // chercher le nombre de caractères pour un br
@@ -53,50 +53,49 @@ for (let index = 0; index < lengthContent; index++) { // on parcourt chaque cara
     }
 
     if (contentDivHtml[index] === '>' && !weHadABR) {
-        weEnteredInABalise = false
-        if (!baliseNotToRegister) { // if it works it is normal to have a text area who is empty, it means, all the balises has been closed
-            svgBalise.push(singleBalise)
+        weEnteredInATag = false
+        if (!tagNotToRegister) { // if it works it is normal to have a text area who is empty, it means, all the Tags has been closed
+            svgTag.push(singleTag)
         }
-        baliseNotToRegister = false;
-        svgTextAndBalises.push(singleBalise)
-        singleBalise = ""
+        tagNotToRegister = false;
+        svgTextAndTags.push(singleTag)
+        singleTag = ""
     }
 
     if (contentDivHtml[index] === '<') {
-        singleBalise = contentDivHtml[index]
-        weEnteredInABalise = true;
-        svgTextAndBalises.push(textToAddToSvg)
+        singleTag = contentDivHtml[index]
+        weEnteredInATag = true;
+        svgTextAndTags.push(textToAddToSvg)
         textToAddToSvg = '';
     }
 
-    if (!weEnteredInABalise && contentDivHtml[index] !== '>' && !regexSpace.test(contentDivHtml[index])) { // here there were an issue because we took the > when weEnteredInABalise was false
+    if (!weEnteredInATag && contentDivHtml[index] !== '>' && !regexSpace.test(contentDivHtml[index])) { // here there were an issue because we took the > when weEnteredInATag was false
         textToAddToSvg = textToAddToSvg + contentDivHtml[index]
-        cmptString++; // we can't be in the middle of a balise because we don't count them
+        cmptString++; // we can't be in the middle of a Tag because we don't count them
     }
 
-    if (regexSpace.test(contentDivHtml[index]) && !weEnteredInABalise) {
-        svgTextAndBalises.push(textToAddToSvg)
-        svgTextAndBalises.push(contentDivHtml[index])
+    if (regexSpace.test(contentDivHtml[index]) && !weEnteredInATag) {
+        svgTextAndTags.push(textToAddToSvg)
+        svgTextAndTags.push(contentDivHtml[index])
         textToAddToSvg = '';
     }
 
     // Faudrait que je compte chaque espace et que je splice
-    if (cmptString > 2100) { // we can't be in the middle of a balise because we don't count them
+    if (cmptString > 2100) { // we can't be in the middle of a Tag because we don't count them
         let stringAllText = '';
-        for (let index = 0; index < svgTextAndBalises.length; index++) {
-            stringAllText = stringAllText + svgTextAndBalises[index]
+        for (let index = 0; index < svgTextAndTags.length; index++) {
+            stringAllText = stringAllText + svgTextAndTags[index]
         }
-        let allBaliseLeft = ''
-        for (let index = 0; index < svgBalise.length; index++) {
-            allBaliseLeft = stringAllText + svgBalise[index]
+        for (let index = 0; index < svgTag.length; index++) {
+            allTagLeft = stringAllText + svgTag[index]
         }
 
-        /* if (svgBalise) { // si certaines balises ne sont pas fermées, les rajouter à la fin en modifiant svgbalise par des balises fermantes ? Peut être que innerHTML ferme les balises
-            for (let index = 0; index < svgBalise.length; index++) {
-                //pageWhereToAddContent.innerHTML = pageWhereToAddContent.innerHTML + svgBaliseModifiedWithEnd[index];
+        /* if (svgTag) { // si certaines Tags ne sont pas fermées, les rajouter à la fin en modifiant svgTag par des Tags fermantes ? Peut être que innerHTML ferme les Tags
+            for (let index = 0; index < svgTag.length; index++) {
+                //pageWhereToAddContent.innerHTML = pageWhereToAddContent.innerHTML + svgTagModifiedWithEnd[index];
             }
         } */
-        pageWhereToAddContent.innerHTML = stringAllText // inner HTML semble fermer les balises ouvertes
+        pageWhereToAddContent.innerHTML = stringAllText // inner HTML semble fermer les Tags ouvertes
 
         let newPage = document.createElement('div')
         newPage.classList.add('page')
@@ -108,8 +107,8 @@ for (let index = 0; index < lengthContent; index++) { // on parcourt chaque cara
 
         pageWhereToAddContent.innerHTML = ""
         cmptString = 0
-        svgTextAndBalises.length = 0
-        needToAddSvgBaliseInNewSvgTextAndBalise = true // allow to keep the balises whhich haven't been closed and put them in the start of the new page (see the beginning of the function)
+        svgTextAndTags.length = 0
+        needToAddSvgTagInNewSvgTextAndTag = true // allow to keep the Tags whhich haven't been closed and put them in the start of the new page (see the beginning of the function)
     }
 
     weHadABR = false
@@ -118,8 +117,8 @@ for (let index = 0; index < lengthContent; index++) { // on parcourt chaque cara
 
 if (cmptString < 2100) { // last page
     let stringAllText = '';
-    for (let index = 0; index < svgTextAndBalises.length; index++) {
-        stringAllText = stringAllText + svgTextAndBalises[index]
+    for (let index = 0; index < svgTextAndTags.length; index++) {
+        stringAllText = stringAllText + svgTextAndTags[index]
     }
     pageWhereToAddContent.innerHTML = stringAllText
 }
