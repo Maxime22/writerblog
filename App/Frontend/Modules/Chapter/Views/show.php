a8ff23db8e2071cefe0ce6b0bd2d6d095b44ecc8
unknown <sabouretmaxime@gmail.com>
2019-03-07 15:49:16 +0100

    turn works with my own JS, didn't take care of the html, <, and different cut

--- App/Frontend/Modules/Chapter/Views/show.php
+++ App/Frontend/Modules/Chapter/Views/show.php
@@ -2,20 +2,11 @@
 <!-- <div class="col-12 chapterDiv">
 <p><?/*= nl2br($chapter['content']) */?></p>
 </div> -->
-
+<div id ="contentDiv"><?= nl2br($chapter['content']) ?></div>
 <div class="col-12 containerBook">
 <div id="flipbook">
 	<div class="hard cover"><?=$chapter['title']?></div>
-	<div class="page"><div style="padding:40px;"><?= nl2br($chapter['content']) ?></div></div>
-	<div class="page"><div style="padding:40px;">Hoc inmaturo interitu ipse quoque sui pertaesus excessit e vita aetatis nono anno atque vicensimo cum quadriennio imperasset. 
-  natus apud Tuscos in Massa Veternensi, patre Constantio Constantini fratre imperatoris, matreque Galla sorore Rufini et Cerealis, quos trabeae consulares 
-  nobilitarunt et praefecturae.
-Denique Antiochensis ordinis vertices sub uno elogio iussit occidi ideo efferatus, quod ei celebrari vilitatem intempestivam urgenti, cum inpenderet inopia, 
-gravius rationabili responderunt; et perissent ad unum ni comes orientis tunc Honoratus fixa constantia restitisset.
-</div>
-</div>
-	<div class="page">Page 3 </div>
-	<div class="page">Page 4 </div>
+	<div id="firstPage" class="page"><div class="insidePage"></div></div>
 </div>
 <div class="dateAuthorChapter"><p>Par <em><?=$chapter['author']?></em>, le <?=$chapter['addDate']->format('d/m/Y à H\hi')?></p></div>
 </div>
@@ -58,6 +49,7 @@ foreach ($comments as $comment) {
 
 <p><a href="/chapters">Revenir aux chapitres</a></p>
 
-
-<script src="/js/turn.js"></script>
 <script src="/js/book.js"></script>
+<script src="/js/turn.js"></script>
+<script src="/js/bookLogic.js"></script>
+
---
+++ Web/js/bookLogic.js
@@ -0,0 +1,5 @@
+$("#flipbook").turn({
+    width: 1100,
+    height: 600,
+    autoCenter: true
+});
