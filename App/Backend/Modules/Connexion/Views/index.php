5c2b326e6f888fc0297db0c0429be342af282888
unknown <sabouretmaxime@gmail.com>
2019-03-10 12:18:33 +0100

    change front of the connexion page and the chapters page

--- App/Backend/Modules/Connexion/Views/index.php
+++ App/Backend/Modules/Connexion/Views/index.php
@@ -1,11 +1,17 @@
-<h2>Connexion</h2>
+<h2 class ='connexionTitle'>Connexion</h2>
 
+<div class="row">
+<div class="offset-4 col-4">
 <form action="" method="post">
-  <label>Pseudo</label>
-  <input type="text" name="login" /><br />
-  
-  <label>Mot de passe</label>
-  <input type="password" name="password" /><br /><br />
-  
-  <input type="submit" value="Connexion" />
+<div class="form-group">
+  <label for="inputPseudo">Pseudo</label>
+  <input type="text" id="inputPseudo" name="login" class="form-control" /><br />
+  </div>
+  <div class="form-group">
+  <label for="inputPassword">Mot de passe</label>
+  <input type="password" id="for=inputPassword" name="password" class="form-control" /><br /><br />
+  </div>
+  <input type="submit" class="btn btn-primary" value="Connexion" />
 </form>
+</div>
+</div>
--- App/Frontend/Modules/Chapter/Views/index.php
+++ App/Frontend/Modules/Chapter/Views/index.php
@@ -1,14 +1,45 @@
-<h1>Chapitres</h1>
-<?php if (!empty($listChapters)){ ?>
-<table class="tableChapters">
-  <tr><th>Titre</th><th>Lecture</th></tr>
-<?php
+<div class="row">
+<figure class="figure100">
+  <img id="imgMountain" src="/images/montagne.jpg" alt="Image Montagne" title="Montagne"/>
+  <figcaption class="figcaptionMountain">
+    <h1>Chapitres<h1>
+    </figcaption>
+</figure>
+<div class="container">
+<?php 
+$cpt = 0;
+if (!empty($listChapters)) {
 foreach ($listChapters as $chapter) {
-    echo '<tr><td>', htmlspecialchars($chapter['title']), '</td><td><a href="/chapter-', $chapter['id'], '">Lire</a></td></tr>', "\n";
+  $cpt++;
+  ?>
+<div class="row containChapter">
+  <div class="col-2">
+  <a class="linkLittleBookChapters" href="/chapter-<?php echo $chapter['id'] ?>">
+  <div class="littleBookChapters">
+  <h2><?php echo 'Chapitre n°',$cpt,'<br><br>',htmlspecialchars($chapter['title']) ?></h2>
+</div>
+</a>
+</div>
+<div class="col-10">
+  <?php
+  if (strlen($chapter->content()) > 1000) // if chapter->content() has too much letters, we modify it with substr()
+  {
+      $begin = substr($chapter->content(), 0, 1000);
+      $begin = substr($begin, 0, strrpos($begin, ' ')) . '...';
+
+      $chapter->setContent($begin);
+  }
+  echo '<div class="resumeChapters">',($chapter['content']),' <a href="/chapter-',$chapter['id'],'">Découvrir le chapitre...</a></div>';
+  ?>
+  
+</div>
+</div>
+    <?php
 }
-?>
-</table>
-<?php }else{ ?>
+} else {?>
   <span>Aucun chapitre n'a été publié pour le moment... soyez patient, il n'y en a plus pour longtemps !</span>
 <?php
 }
+?>
+</div>
+</div>
